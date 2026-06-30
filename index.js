14d93e77d6b48600e89676bba5cdf36cbd534ba5
Sujalparmar2709 <sujalparmar2709@gmail.com>
2026-06-30 13:22:14 +0530

    using EJS to make a frontend side

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
 const { connectToMongoDB } = require("./connect");
 const urlRouter = require("./routes/url");
+const staticRouter = require('./routes/staticRouter');
 const URL = require('./models/url');
+
 const app = express();
 const PORT = 8001;
 
@@ -10,23 +13,29 @@ connectToMongoDB("mongodb://localhost:27017/short-url")
     console.log("mongoDB is Connected")
 );
 
+app.set("view engine", "ejs");
+app.set('views', path.resolve("./views"))
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+
 app.use("/url", urlRouter);
+app.use("/", staticRouter);
 
 
-app.get("/:shortId", async (req, res) =>{
+app.get("/url/:shortId", async (req, res) =>{
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate(
         {
             shortId
         },
         { 
-            $push:{
-                visitHostory:{
-                    timestamp:Date.now(),
+            $push: {
+                visitHistory: {
+                    timestamp: Date.now(),
                 },
-            }, 
+            },
         }
     );  
   res.redirect(entry.redirectURL);
